3b97d410b894adbb2354a6a5635c52cd7150badd
Passenger108 <157837251+Passenger108@users.noreply.github.com>
2025-07-22 14:26:22 +0530

    init: React Hooks wallet  with Vite
    ''

---
+++ README.md
@@ -0,0 +1,4 @@
+### React-hooks-Wallet 
+    > 🚧 **This project is a React utility library and is currently under construction. Stay tuned for updates!**
+
+Developing ...
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+// Temporary dev playground
+const App = () => {
+  return <h1>React Hooks Wallet</h1>
+}
+
+createRoot(document.getElementById('root'))
+.render(
+    <App />
+)
